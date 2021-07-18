bf664060e8061b64536faa20866a1a5c05b6d842
K-nunuhara <k.nunuhara@gmail.com>
2021-07-18 19:19:15 +0900

    update doc

--- SampleSite/scripts/main.js
+++ SampleSite/scripts/main.js
@@ -2,15 +2,15 @@ var items = document.getElementsByTagName('li');
 var itemArray = Array.prototype.slice.call(items);
 
 itemArray.forEach(function(element) {
-    element.addEventListener('mouseover', function(event) {
+    element.addEventListener('mouseover', function() {
         element.style.background = '#0091EA';
     });
 
-    element.addEventListener('mouseleave', function(event) {
+    element.addEventListener('mouseleave', function() {
         element.style.background = '#000000';
     });
 
-    element.addEventListener('click', function(event) {
+    element.addEventListener('click', function() {
         element.textContent = 'CCC';
     });
 });
